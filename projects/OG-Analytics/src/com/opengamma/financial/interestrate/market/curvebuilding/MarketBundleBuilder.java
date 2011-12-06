3bb8f56af1b469abe6572d757c62c39d94ac6ef3
Marc Henrard <marc@opengamma.com>
2011-12-06 18:02:14 +0000

    [PLAT-1732] Changes to MarketBundleBuilder.

--- projects/OG-Analytics/src/com/opengamma/financial/interestrate/market/curvebuilding/MarketBundleBuilder.java
+++ projects/OG-Analytics/src/com/opengamma/financial/interestrate/market/curvebuilding/MarketBundleBuilder.java
@@ -25,18 +25,14 @@ import com.opengamma.util.money.MultipleCurrencyAmount;
  */
 public class MarketBundleBuilder {
 
+  /**
+   * Absolute and relative tolerance for the root finding process.
+   */
   private static final double EPS = 1e-8;
+  /**
+   * The maximum number of steps for the root finding process.
+   */
   private static final int STEPS = 100;
-
-  public static MarketBundle discounting(final InstrumentDerivative[] instruments, Currency ccy, IndexDeposit[] indexes, Interpolator1D interpolator,
-      InstrumentDerivativeVisitor<Object, Double> nodeTimeCalculator, InstrumentDerivativeVisitor<MarketBundle, MultipleCurrencyAmount> presentValueCalculator) {
-    int nbInstruments = instruments.length;
-    double[] startRate = new double[nbInstruments];
-    for (int loopins = 0; loopins < nbInstruments; loopins++) {
-      startRate[loopins] = 0.025; //TODO: improve the start values.
-    }
-    return discounting(instruments, startRate, ccy, indexes, interpolator, nodeTimeCalculator, presentValueCalculator);
-  }
 
   /**
    * Build a unique discounting curve.
@@ -74,6 +70,26 @@ public class MarketBundleBuilder {
     final DoubleMatrix1D yieldCurveNodes = rootFinder.getRoot(func, new DoubleMatrix1D(startRate));
     MarketBundle market = MarketBundleBuildingFunction.build(data, yieldCurveNodes);
     return market;
+  }
+
+  /**
+   * Build a unique discounting curve. The start rate for the root finder process are set at 2.5%.
+   * @param instruments The instruments use to build the curve.
+   * @param ccy The curve currency.
+   * @param indexes The IndexDeposit for which the discounting curve is used to estimate the forward (usually an OIS index).
+   * @param interpolator The interpolator used for the curve.
+   * @param nodeTimeCalculator The calculator to compute the curve node from the instruments.
+   * @param presentValueCalculator The calculator used to compute the instruments present value.
+   * @return The build market bundle.
+   */
+  public static MarketBundle discounting(final InstrumentDerivative[] instruments, Currency ccy, IndexDeposit[] indexes, Interpolator1D interpolator,
+      InstrumentDerivativeVisitor<Object, Double> nodeTimeCalculator, InstrumentDerivativeVisitor<MarketBundle, MultipleCurrencyAmount> presentValueCalculator) {
+    int nbInstruments = instruments.length;
+    double[] startRate = new double[nbInstruments];
+    for (int loopins = 0; loopins < nbInstruments; loopins++) {
+      startRate[loopins] = 0.025;
+    }
+    return discounting(instruments, startRate, ccy, indexes, interpolator, nodeTimeCalculator, presentValueCalculator);
   }
 
 }
